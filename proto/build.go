a240b3cf2f6a7366d00c022d02be784e29891910
io6696 <buicongtam96@gmail.com>
2023-02-16 15:51:19 +0700

    add bandarqq proto

--- proto/build.go
+++ proto/build.go
@@ -21,3 +21,4 @@ package api
 //go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative match_api.proto
 //go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative auth_api.proto
 //go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative wallet.proto
+//go:generate protoc --proto_path=. --go_out=. --go_opt=paths=source_relative bandarqq_api.proto
