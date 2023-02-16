4a86b58b765b1900cce36bc54e8517b90d30818c
DANGQUOCSON <dangquocson1995@gmail.com>
2023-02-16 20:31:46 +0700

    [update]: update interface engine

--- lib/engine.go
+++ lib/engine.go
@@ -3,6 +3,6 @@ package lib
 type Engine interface {
 	NewGame(matchState interface{}) (interface{}, error)
 	Random(min, max int) int
-	Process(matchState interface{}) interface{}
-	Finish(matchState interface{}) interface{}
+	Process(matchState interface{}) (interface{}, error)
+	Finish(matchState interface{}) (interface{}, error)
 }
