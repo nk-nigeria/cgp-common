61db2746b88547b1b7108df32fbb4d150200b38c
sondq <sondq@vng.com.vn>
2023-05-10 16:40:56 +0700

    [update]

--- lib/operation.go
+++ lib/operation.go
@@ -1,8 +1,30 @@
 package lib
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
 	pb "github.com/ciaolink-game-platform/cgp-common/proto"
 )
+
+var httpClient *http.Client
+var defaultHttpKey string = ""
+
+func init() {
+	t := http.DefaultTransport.(*http.Transport).Clone()
+	t.MaxIdleConns = 100
+	t.MaxConnsPerHost = 100
+	t.MaxIdleConnsPerHost = 100
+
+	httpClient = &http.Client{
+		Timeout:   10 * time.Second,
+		Transport: t,
+	}
+}
 
 type reportGame struct {
 	users []*pb.PlayerData
@@ -18,9 +40,26 @@ func NewReportGame() *reportGame {
 	return o
 }
 
-// save to db
-func (o *reportGame) Commit() error {
-	return nil
+// call persistent data
+func (o *reportGame) Commit(host string) ([]byte, int, error) {
+	targetUrl := fmt.Sprintf("%s/v2/console/api/endpoints/rpc/op-report", host)
+	data, _ := json.Marshal(o)
+	req, err := http.NewRequest("POST", targetUrl, bytes.NewReader(data))
+	if err != nil {
+		return nil, 0, err
+	}
+	req.Header.Add("defaultkey", defaultHttpKey)
+	// res, err := httpClient.Post(targetUrl, http.DetectContentType(data), bytes.NewReader(data))
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+	var bodyRes []byte
+	if res != nil {
+		bodyRes, err = io.ReadAll(res.Body)
+		res.Body.Close()
+	}
+	return bodyRes, res.StatusCode, err
 }
 
 func (o *reportGame) AddPlayerDate(data *pb.PlayerData) *reportGame {
