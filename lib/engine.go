baf5b261014f3c5ecceeee54b6c9b368f69865a8
sondq <quocson@gmail.com>
2023-06-15 21:40:55 +0700

    [update]: add engine api

--- lib/engine.go
+++ lib/engine.go
@@ -5,4 +5,6 @@ type Engine interface {
 	Random(min, max int) int
 	Process(matchState interface{}) (interface{}, error)
 	Finish(matchState interface{}) (interface{}, error)
+	// for sync with loop game of nakame. return if nothing todo
+	Loop(matchState interface{}) (interface{}, error)
 }
