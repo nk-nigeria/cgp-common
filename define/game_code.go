c9c0377e83097134bb98dc9fb921ecebcc0e948c
qs <abc@gmail.com>
2023-08-23 19:38:43 +0700

    add crytorushGame

--- define/game_code.go
+++ define/game_code.go
@@ -1,21 +1,28 @@
 package define
 
-const (
-	SixiangGameName = "sixiang"
-	TarzanGameName  = "tarzan"
-	JuicyGardenName = "juicygarden"
-	IncaGameName    = "inca"
+type GameName string
 
-	BlackjackName   = "blackjack"
-	BandarqqName    = "bandarqq"
-	SicboName       = "sicbo"
-	BaccaratName    = "baccarat"
-	ColorGameName   = "color-game"
-	DragontigerName = "dragontiger"
+const (
+	SixiangGameName GameName = "sixiang"
+	TarzanGameName  GameName = "tarzan"
+	JuicyGardenName GameName = "juicygarden"
+	// clone game JuicyGardenName
+	CrytoRush    GameName = "cryotorush"
+	IncaGameName GameName = "inca"
+
+	BlackjackName   GameName = "blackjack"
+	BandarqqName    GameName = "bandarqq"
+	SicboName       GameName = "sicbo"
+	BaccaratName    GameName = "baccarat"
+	ColorGameName   GameName = "color-game"
+	DragontigerName GameName = "dragontiger"
 )
 
+func (g *GameName) String() string {
+	return string(*g)
+}
 func IsAllowJoinInGameOnProgress(gameCode string) bool {
-	switch gameCode {
+	switch GameName(gameCode) {
 	case SixiangGameName, TarzanGameName, JuicyGardenName:
 		return false
 	default:
