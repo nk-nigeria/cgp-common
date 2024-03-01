0adc1465a9b00768d8c8422b8166a9852ac83e8c
sondq <sondq@gmail.com>
2024-03-01 17:21:24 +0700

    fix

--- bot/rule_lucky_bet.go
+++ bot/rule_lucky_bet.go
@@ -3,23 +3,20 @@ package bot
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
-	"errors"
-	"fmt"
 
 	"github.com/heroiclabs/nakama-common/runtime"
 )
 
 type LuckyBet struct {
 	// UserId    string `json:"user_id,omitempty"`
-	ChipWin   int `json:"chip_win,omitempty"`
-	ChipSpent int `json:"chip_spent,omitempty"`
-	CoRate    float64
-	Ci        float64
-	CoInDay   float64
-	// internal
-	chipWinChange   int
-	chipSpentChange int
+	// ChipWin   int `json:"chip_win,omitempty"`
+	// ChipSpent int `json:"chip_spent,omitempty"`
+	CoRate  float64
+	Ci      float64
+	CoInDay float64
+	// // internal
+	// chipWinChange   int
+	// chipSpentChange int
 }
 
 type RuleLuckyBet struct {
@@ -42,10 +39,10 @@ func (l *RuleLuckyBet) UpdateUser(userId string, rtp LuckyBet) {
 		return
 	}
 	currentRtp := l.rules[userId]
-	currentRtp.ChipSpent += rtp.ChipSpent
-	currentRtp.ChipWin += rtp.ChipWin
-	currentRtp.chipSpentChange += rtp.ChipSpent
-	currentRtp.chipWinChange += rtp.ChipWin
+	// currentRtp.ChipSpent += rtp.ChipSpent
+	// currentRtp.ChipWin += rtp.ChipWin
+	// currentRtp.chipSpentChange += rtp.ChipSpent
+	// currentRtp.chipWinChange += rtp.ChipWin
 	if rtp.Ci > 0 {
 		currentRtp.Ci = rtp.Ci
 	}
@@ -74,8 +71,8 @@ func (l *RuleLuckyBet) avg() LuckyBet {
 		lucky.Ci += v.Ci
 		lucky.CoInDay += v.CoInDay
 		lucky.CoRate += v.CoRate
-		lucky.ChipWin += v.ChipWin
-		lucky.ChipSpent += v.ChipSpent
+		// lucky.ChipWin += v.ChipWin
+		// lucky.ChipSpent += v.ChipSpent
 	}
 	return lucky
 }
@@ -87,45 +84,8 @@ func (l *RuleLuckyBet) LoadUser(ctx context.Context,
 	userId string) {
 }
 
-func (l *RuleLuckyBet) GetBaseAction() BaseAction {
+func (l *RuleLuckyBet) GetBaseAction(totalWin int) BaseAction {
 	lucky := l.avg()
 	cfgBet := l.tableCfg.GetConfig(lucky.CoRate, lucky.Ci, lucky.CoInDay)
-	totalWin := lucky.ChipWin - lucky.ChipSpent
 	return cfgBet.GetBaseAction(int64(totalWin))
-}
-
-func (l *RuleLuckyBet) SaveToProfile(
-	ctx context.Context,
-	logger runtime.Logger,
-	nk runtime.NakamaModule,
-	db *sql.DB,
-	userId string,
-	gameCode string,
-) error {
-	if IsBot(userId) {
-		return nil
-	}
-	rtp, exist := l.rules[userId]
-	if !exist {
-		return errors.New("not exist")
-	}
-	if rtp.chipSpentChange == 0 && rtp.chipWinChange == 0 {
-		// not change, no need save
-		return nil
-	}
-	data, err := json.Marshal(rtp)
-	if err != nil {
-		return err
-	}
-	query := `UPDATE
-					users AS u
-				SET
-					metadata
-						= u.metadata
-						|| jsonb_build_object(` + fmt.Sprintf("'rtp.%s'", gameCode) + `, '` + string(data) + `')
-				WHERE	
-					id = $1;`
-	_, err = db.ExecContext(ctx, query, userId)
-	l.RemoveUser(userId)
-	return err
 }
