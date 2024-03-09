5b325b4d9b528ed44e18bcc459e6452401396bae
sondq <sondq@gmail.com>
2024-03-09 17:34:34 +0700

    add define event match end

--- define/event.go
+++ define/event.go
@@ -6,3 +6,9 @@ const (
 	EventName_Game_Report EventName = "event_game_report"
 	EventName_TopUp       EventName = "event_top_up"
 )
+
+type NakEvent string
+
+const (
+	NakEventMatchEnd NakEvent = "nakama_match_end"
+)
